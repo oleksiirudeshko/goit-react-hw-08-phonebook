09c17508180d3994343d08c3687206cf20a992b3
Oleksii <clothes.lyks@gmail.com>
2020-11-01 15:32:29 +0200

    Updates

---
+++ static/js/5.56f38422.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[5],{134:function(e,t,n){"use strict";n.r(t);var c=n(2);n(0);t.default=function(){return Object(c.jsx)("div",{style:{width:"500px"},children:Object(c.jsxs)("ul",{children:[Object(c.jsx)("h2",{children:"Phonebook"}),Object(c.jsx)("li",{children:"Find and add your contacts by name"})]})})}}}]);
+//# sourceMappingURL=5.56f38422.chunk.js.map
